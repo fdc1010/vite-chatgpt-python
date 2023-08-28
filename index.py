6e54dbf2651d868087851ff6716012c2c2788cef
franklin caruana <franklincaruana@gmail.com>
2023-08-28 16:30:42 +0800

    import cors

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 import os
-import openai
 from flask import Flask, request
+from flask_cors import CORS
+import openai
 
 app = Flask(__name__)
+CORS(app)
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/')
